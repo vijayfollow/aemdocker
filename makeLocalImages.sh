bb5429d9b2b928f010ba335f8332a6b7bdcab361
vijayfollow <42770772+vijayfollow@users.noreply.github.com>
2019-02-20 15:30:02 +0530

    Update makeLocalImages.sh

--- makeLocalImages.sh
+++ makeLocalImages.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-function getinput {
-  while true; do
-	  echo "Put your AEM jar and your license file in ${1}-tar/resources directory."
-      read -p "When ready type y and hit enter or q to quit? " y
-      case $y in
-          [Yy]* ) break;;
-          [Qq]* ) exit 0;;
-          * ) echo "Please answer y when your ready to move foward.";;
-      esac
-  done
-}
+
 
 function build {
   sh -c 'cd '${1}'/ && exec docker build -t aem_6-2_'$(echo ${1} | sed 's/-.*//')' .'
@@ -19,13 +9,13 @@ function build {
 
 build "base"
 
-getinput "publish"
+
 build "publish-tar"
 
-getinput "author"
+
 build "author-tar"
 
 build "dispatcher-ps"
 
-sh -c 'cd composedev-tar'
-echo 'Now go into composedev-tar and run this command:./start-containers.sh'
+
+echo 'All 3 Images are build'
